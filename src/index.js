9b17537df9e6b9eeea6ef1f30c56843ae1496b8c
Afiq Ramli <m.afiqrmli@gmail.com>
2017-10-12 15:14:11 +0800

    Add core files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Youtube API Key to use YT search services
+const API_KEY = 'AIzaSyBF_s_Xf5wRZmsyeZ8-0trBK6CxBWOJeD0';
+// 
+
+const Layout = () => <h1>Just another react app</h1>
+
+ReactDOM.render(
+  <Layout/>, document.getElementById('container')
+);
